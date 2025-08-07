08aee2ab4ed2cd14b15060a0100dc760196b17b7
Sabbella Laharika <sabbellalaharika05@gmail.com>
2025-08-07 21:17:27 +0530

    Updated Projects

--- projects/project.js
+++ projects/project.js
@@ -16,14 +16,15 @@ const Projects = [
             `<img src="https://img.icons8.com/color/48/000000/mongodb.png"/>`,
             `<img src="https://img.icons8.com/color/48/000000/express.png"/>`
         ],
-        project_link : "https://agoda-clone-website.vercel.app/",
-        project_type : "Team Project"
+        project_link : "",
+        project_type : "Team Project",
+        isLive : true
     },
     {
-        name : "ML WebDisease Cancer Prediction with Precision",
+        name : "ML Web Disease Cancer Prediction with Precision",
         about : "ML web application for disease and cancer prediction, to encompass data processing and model deployment.",
-        img : "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT83waNZy94ppQK-oRhV70lco-r4kcA8rZ13Q&usqp=CAU",
-        gitrepo : "https://github.com/db648/Specs-Makers-clone",
+        img : "",
+        gitrepo : "",
         blog : "https://medium.com/@vdbhavani.648/specsmakers-clone-8e0434e3ed0e",
         tech_stack : "HTML | CSS | JAVASCRIPT | API | JSON-SERVER",
         tech_stackicons : [
@@ -33,14 +34,15 @@ const Projects = [
         `<img src="https://img.icons8.com/nolan/64/api-settings.png" width="48px"/>`,
         `<img src="https://img.icons8.com/windows/48/000000/node-js.png"/>`
         ],
-        project_link : "https://tender-hoover-a53af4.netlify.app/",
-        project_type : "Team Project"
+        project_link : "",
+        project_type : "Team Project",
+        isLive : true
     },
     {
         name : "Hospital Pre-Booking Web Application",
         about : "Web application for hospital pre-bookings, to streamline the appointments and administrative tasks.",
         img : "https://content.shop4reebok.com/static/hero/Sneeker_Fest_1920x800_091221_2.jpg",
-        gitrepo : "https://github.com/db648/reebok",
+        gitrepo : "",
         blog : "https://medium.com/@suraj.devere/shop4reebok-clone-eddfa7c32fe7",
         tech_stack : "HTML | CSS | JAVASCRIPT",
         tech_stackicons : [
@@ -49,15 +51,16 @@ const Projects = [
             `<img src="https://img.icons8.com/color/50/000000/javascript--v2.png"/>`,
             
         ],
-        project_link : "https://rebookwebsite-clone1.vercel.app/",
-        project_type : "Team Project"
+        project_link : "",
+        project_type : "Team Project",
+        isLive : true
 
     },
     {
         name : "Zoom-like App Emotion Detection of Students",
         about : "Video conferencing application with real-time student emotion detection using advanced computer vision and machine learning.",
-        img : "./homepage-shopping.png",
-        gitrepo : "https://github.com/db648/ecommerce-react-project",
+        img : "",
+        gitrepo : "",
         blog : "",
         tech_stack : "HTML | CSS | JAVASCRIPT | React | Redux | MongoDB | Express | Nodejs | Bootstrap",
         tech_stackicons : [
@@ -72,15 +75,16 @@ const Projects = [
             `<img src="https://img.icons8.com/color/48/000000/bootstrap.png"/>`
             
         ],
-        project_link : "https://ecommerce-react-project.vercel.app/",
-        project_type : "Individual Project"
+        project_link : "",
+        project_type : "Team Project",
+        isLive : true
 
     },
     {
         name : "Smart Stick",
         about : "Smart Stick for enhancing independence and safety for visually impaired users through real-time obstacle detection, navigation assistance, and environmental awareness.",
-        img : "./weather.png",
-        gitrepo : "https://github.com/db648/weatherapp",
+        img : "./images/SmartStick.jpg",
+        gitrepo : "",
         blog : "",
         tech_stack : "HTML | CSS | JAVASCRIPT | API's",
         tech_stackicons : [
@@ -90,44 +94,45 @@ const Projects = [
             `<img src="https://img.icons8.com/nolan/64/api-settings.png" width="48px"/>`
             
         ],
-        project_link : "https://weatherapp-ten-kappa.vercel.app/",
-        project_type : "Individual Project"
+        project_link : "",
+        project_type : "Team Project",
+        isLive : false
 
     },
     {
         name : "Data Structures Visualizer & Quiz System",
         about : "An interactive application for visualizing sorting and search algorithms, stacks, queues, linked lists, trees and graphs with animated effects and also included a quiz.",
-        img : "./weather.png",
-        gitrepo : "https://github.com/db648/weatherapp",
+        img : "./images/DSQ1.png",
+        gitrepo : "https://github.com/SabbellaLaharika/DS_Visualization_and_QUIZ_App",
         blog : "",
-        tech_stack : "HTML | CSS | JAVASCRIPT | API's",
+        tech_stack : "JAVA | MYSQL | GIT",
         tech_stackicons : [
-            `<img src="https://img.icons8.com/color/50/000000/html-5--v1.png" />`,
-            `<img src="https://img.icons8.com/color/50/000000/css3.png"/>`,
-            `<img src="https://img.icons8.com/color/50/000000/javascript--v2.png"/>`,
-            `<img src="https://img.icons8.com/nolan/64/api-settings.png" width="48px"/>`
+            `<img src="https://img.icons8.com/?size=50&id=13679&format=png&color=000000" />`,
+            `<img src="https://img.icons8.com/?size=50&id=UFXRpPFebwa2&format=png&color=000000"/>`,
+            `<img src="https://img.icons8.com/?size=50&id=20906&format=png&color=000000"/>`
             
         ],
-        project_link : "https://weatherapp-ten-kappa.vercel.app/",
-        project_type : "Individual Project"
+        project_link : "https://drive.google.com/file/d/13aung7ansQ5InBO-U4xgNWtOlE9k7msb/view",
+        project_type : "Team Project",
+        isLive : false
 
     },
     {
         name : "CurioCart - E-Commerce Application",
         about : "A real-time e-commerce simulation showcasing comprehensive product management and robust data operations.",
-        img : "./weather.png",
-        gitrepo : "https://github.com/db648/weatherapp",
+        img : "./images/CurioCart1.png",
+        gitrepo : "https://github.com/SabbellaLaharika/CurioCart-E-Commerce_Application_Using_Java_Modified",
         blog : "",
-        tech_stack : "HTML | CSS | JAVASCRIPT | API's",
+        tech_stack : "JAVA | MYSQL | GIT",
         tech_stackicons : [
-            `<img src="https://img.icons8.com/color/50/000000/html-5--v1.png" />`,
-            `<img src="https://img.icons8.com/color/50/000000/css3.png"/>`,
-            `<img src="https://img.icons8.com/color/50/000000/javascript--v2.png"/>`,
-            `<img src="https://img.icons8.com/nolan/64/api-settings.png" width="48px"/>`
+            `<img src="https://img.icons8.com/?size=50&id=13679&format=png&color=000000" />`,
+            `<img src="https://img.icons8.com/?size=50&id=UFXRpPFebwa2&format=png&color=000000"/>`,
+            `<img src="https://img.icons8.com/?size=50&id=20906&format=png&color=000000"/>`
             
         ],
-        project_link : "https://weatherapp-ten-kappa.vercel.app/",
-        project_type : "Individual Project"
+        project_link : "https://rebrand.ly/041f01",
+        project_type : "Team Project",
+        isLive : false
 
     }
 ]
@@ -144,7 +149,7 @@ Projects.forEach(pro => {
       <img src=${pro.img} alt="">
       <h3 class="project-name"><b>${pro.name}</b></h3> <br/>
       <h4 class="project-name project__about">${pro.about}</h4>
-      <a href=${pro.project_link} target="_blank" class="see-project">Live💻</a>
+      <a href=${pro.project_link} target="_blank" class="see-project">${pro.isLive ? "Live" : "Demo"}💻</a>
       <a href=${pro.gitrepo} target="_blank" class="github-repo">GitHub</a>
       </div>
       <br/>
